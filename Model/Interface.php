245d50f277ba5f49e033987d71dff7c8839696a6
Radosław Zieliński <radoslaw@zielinski.cc>
2012-03-08 16:23:19 +0100

    Changed interface method name from getById() -> get()

--- Model/Interface.php
+++ Model/Interface.php
@@ -1,7 +1,7 @@
 <?php
 interface Crud_Model_Interface {
     function getAll($where = null, $order = null);
-    function getById($id);
+    function get($id);
     function save(array $data, Zend_Db_Table_Row_Abstract $currentItem = null);
     function delete($id);
     function getForm();
